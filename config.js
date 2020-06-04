f564143d6ee407094f95ded62293d18264b7f323
Kyle Pace <kpace2296@gmail.com>
2020-06-03 21:07:14 -0500

    fixed app url again

--- config.js
+++ config.js
@@ -22,7 +22,7 @@ module.exports = {
       clientId: CLIENT_ID,
       clientSecret: CLIENT_SECRET,
       issuer: ISSUER,
-      appBaseUrl: 'https://okta-project.herokuapp.com/',
+      appBaseUrl: 'https://okta-project.herokuapp.com',
       scope: 'openid profile email',
     //   testing: {
     //     disableHttpsCheck: OKTA_TESTING_DISABLEHTTPSCHECK
