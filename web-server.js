5da212b780654c13c380577980b0ff126966929b
Kyle Pace <kpace2296@gmail.com>
2020-06-03 21:15:56 -0500

    working on the session

--- web-server.js
+++ web-server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const session = require('express-session');
+const session = require('connect-session-knex');
 const path = require('path');
 const dotenv = require('dotenv');
 const fs = require('fs');
